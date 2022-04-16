b86f0d22d739bd63203808eaef1a915e212b55eb
superboo0311 <datnguyentien0311@gmail.com>
2022-04-16 19:36:00 -0400

    style homepage

--- power_of_words/lib/screen/homePage.dart
+++ power_of_words/lib/screen/homePage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +7,10 @@ import 'package:power_of_words/auth/authentication_service.dart';
 import 'package:provider/provider.dart';
 import '../model/database.dart';
 import '../extension/string_extension.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:animations/animations.dart';
+import 'package:timelines/timelines.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,7 +22,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    String name;
+    double unitHeightValue = MediaQuery.of(context).size.height * 0.01;
+    double unitWidthValue = MediaQuery.of(context).size.width * 0.01;
+    var purple = Color(0xFF3B1B6A);
+    var notpurple = Color(0XFFD5D4EA);
     String uid;
     final FirebaseAuth _ath = FirebaseAuth.instance;
     final User? user = _ath.currentUser;
@@ -39,32 +48,121 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         body: SafeArea(
       child: Container(
-        padding: EdgeInsets.all(10),
+        width: windowWidth,
+        padding: EdgeInsets.all(20),
         child: Column(children: [
-          Container(
-            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-              stream: FirebaseFirestore.instance
-                  .collection("UserID")
-                  .doc(uid)
-                  .snapshots(),
-              builder: (_, snapshot) {
-                if (snapshot.hasError) return Text('Error = ${snapshot.error}');
-                if (snapshot.hasData) {
-                  var output = snapshot.data!.data();
-                  String first = output!['firstName'];
-                  String last = output['lastName'];
-                  String capfirst = first.capitalize();
-                  String caplast = last.capitalize();
-                  return Text("Welcome $capfirst $caplast");
-                }
-                return Center(child: CircularProgressIndicator());
-              },
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child:
+                        StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                      stream: FirebaseFirestore.instance
+                          .collection("UserID")
+                          .doc(uid)
+                          .snapshots(),
+                      builder: (_, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text('Error = ${snapshot.error}');
+                        }
+                        if (snapshot.hasData) {
+                          var output = snapshot.data!.data();
+                          String gen = output!['gender'];
+                          String race = output['race'];
+                          int age = output['age'];
+                          if (age > 40) {
+                          } else {
+                            return Container(
+                                width: 60,
+                                height: 60,
+                                child: SvgPicture.asset(
+                                    'pic/americanafrican.svg'));
+                          }
+                        }
+                        return Text("Loading");
+                      },
+                    ),
+                  ),
+                  Stack(children: [
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                          fontSize: unitHeightValue * 3,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 3
+                            ..color = purple),
+                    ),
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                          fontSize: unitHeightValue * 3, color: notpurple),
+                    ),
+                  ]),
+                  Container(
+                    child:
+                        StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                      stream: FirebaseFirestore.instance
+                          .collection("UserID")
+                          .doc(uid)
+                          .snapshots(),
+                      builder: (_, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text('Error = ${snapshot.error}');
+                        }
+                        if (snapshot.hasData) {
+                          var output = snapshot.data!.data();
+                          String first = output!['firstName'];
+                          String last = output['lastName'];
+                          String capfirst = first.capitalize();
+                          String caplast = last.capitalize();
+                          return Align(
+                              alignment: Alignment.topLeft,
+                              child: Stack(children: [
+                                Text(
+                                  "$capfirst $caplast !",
+                                  style: TextStyle(
+                                      fontSize: unitHeightValue * 3,
+                                      foreground: Paint()
+                                        ..style = PaintingStyle.stroke
+                                        ..strokeWidth = 3
+                                        ..color = purple),
+                                ),
+                                Text(
+                                  "$capfirst $caplast !",
+                                  style: TextStyle(
+                                      color: notpurple,
+                                      fontSize: unitHeightValue * 3),
+                                )
+                              ]));
+                        }
+                        return Text("Loading");
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              IconButton(
+                icon: const FaIcon(FontAwesomeIcons.rightFromBracket),
+                color: Color(0xFF472973),
+                onPressed: () async {
+                  await authService.signOut();
+                },
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
           ),
           TextField(
               controller: messageController,
-              decoration:
-                  InputDecoration(hintText: "What do you have in mine")),
+              decoration: InputDecoration(
+                  border: new OutlineInputBorder(
+                      borderSide: new BorderSide(color: Colors.teal)),
+                  hintText: "What do you have in mine")),
           ElevatedButton(
               onPressed: (() async {
                 DatabaseService(uid: uid)
@@ -91,17 +189,21 @@ class _HomePageState extends State<HomePage> {
                         String userinput = data.docs[index]['message'];
                         DateTime dateinput = data.docs[index]['date'].toDate();
                         String dateToString = dateinput.toString();
-                        return Text("$dateToString \n $userinput");
+                        return Column(children: <Widget>[
+                          Row(children: <Widget>[
+                            Container(
+                                width: 60,
+                                height: 60,
+                                child: SvgPicture.asset(
+                                    'pic/americanafrican.svg')),
+                            Text("$dateToString \n $userinput"),
+                            SizedBox(
+                              height: 120,
+                            ),
+                          ]),
+                        ]);
                       });
                 }),
-          ),
-          Center(
-            child: ElevatedButton(
-              child: Text('Logout'),
-              onPressed: () async {
-                await authService.signOut();
-              },
-            ),
           ),
         ]),
       ),
